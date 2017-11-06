6e734e93a86d0fa6da039933db03947fe20a0b76
Louis Mathe <mathe.louis@gmail.com>
2017-11-06 17:02:52 +0100

    scrabble

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'game', to: 'plays#game'
+  get 'score', to: 'plays#score'
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
